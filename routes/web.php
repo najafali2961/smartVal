16e754a58bff55270f24b232838a4778ed605751
najjaf <najaf2961@gmail.com>
2025-11-25 21:17:14 +0500

    sdf

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/contact', function () {
+    return view('sections.contact');
+})->name('contact');
+Route::get('/blog', function () {
+    return view('sections.blog');
+})->name('blog');
+Route::get('/about', function () {
+    return view('sections.about');
+})->name('about');
+Route::get('/service', function () {
+    return view('sections.services');
+})->name('services');
