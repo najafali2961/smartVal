0c7c62e9044341a857fa7d549d59dffb752330c8
najjaf <najaf2961@gmail.com>
2025-11-29 21:42:20 +0500

    dfg

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,75 +1,97 @@
- <div class="wrap">
-     <div class="container">
-         <div class="row">
-             <div class="col-md-12">
-                 <div class="bg-wrap">
-                     <div class="row">
-                         <div class="col-md-6 d-flex align-items-center">
-                             <p class="mb-0 phone pl-md-2">
-                                 <a href="#" class="mr-2"><span class="fa fa-phone mr-1"></span> +00 1234
-                                     567</a>
-                                 <a href="#"><span class="fa fa-paper-plane mr-1"></span>
-                                     youremail@email.com</a>
-                             </p>
-                         </div>
-                         <div class="col-md-6 d-flex justify-content-md-end">
-                             <div class="social-media">
-                                 <p class="mb-0 d-flex">
-                                     <a href="#" class="d-flex align-items-center justify-content-center"><span
-                                             class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
-                                     <a href="#" class="d-flex align-items-center justify-content-center"><span
-                                             class="fa fa-twitter"><i class="sr-only">Twitter</i></span></a>
-                                     <a href="#" class="d-flex align-items-center justify-content-center"><span
-                                             class="fa fa-instagram"><i class="sr-only">Instagram</i></span></a>
-                                     <a href="#" class="d-flex align-items-center justify-content-center"><span
-                                             class="fa fa-dribbble"><i class="sr-only">Dribbble</i></span></a>
-                                 </p>
-                             </div>
-                         </div>
-                     </div>
-                 </div>
-             </div>
-         </div>
-     </div>
- </div>
- <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-     <div class="container">
-         <a class="navbar-brand" href="index.html">Accounting</a>
-         <form action="#" class="searchform order-sm-start order-lg-last">
-             <div class="form-group d-flex">
-                 <input type="text" class="form-control pl-3" placeholder="Search">
-                 <button type="submit" placeholder="" class="form-control search"><span
-                         class="fa fa-search"></span></button>
-             </div>
-         </form>
-         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
-             aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-             <span class="fa fa-bars"></span> Menu
-         </button>
-         <div class="collapse navbar-collapse" id="ftco-nav">
-             <ul class="navbar-nav m-auto">
-                 <li class="nav-item {{ Route::is('home') ? 'active' : '' }}">
-                     <a href="{{ route('home') }}" class="nav-link">Home</a>
-                 </li>
-                 <li class="nav-item {{ Route::is('about') ? 'active' : '' }}">
-                     <a href="{{ route('about') }}" class="nav-link">About</a>
-                 </li>
-                 <li class="nav-item {{ Route::is('services') ? 'active' : '' }}">
-                     <a href="{{ route('services') }}" class="nav-link">Services</a>
-                 </li>
-                 <li class="nav-item {{ Route::is('cases') ? 'active' : '' }}">
-                     <a href="{{ route('cases') }}" class="nav-link">Case Study</a>
-                 </li>
-                 <li class="nav-item {{ Route::is('blog') ? 'active' : '' }}">
-                     <a href="{{ route('blog') }}" class="nav-link">Blog</a>
-                 </li>
-                 <li class="nav-item {{ Route::is('contact') ? 'active' : '' }}">
-                     <a href="{{ route('contact') }}" class="nav-link">Contact</a>
-                 </li>
-             </ul>
+<div class="wrap">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="bg-wrap">
+                    <div class="row">
+                        <div class="col-md-6 d-flex align-items-center">
+                            <p class="mb-0 phone pl-md-2">
+                                <a href="#" class="mr-2"><span class="fa fa-phone mr-1"></span> +00 1234 567</a>
+                                <a href="#"><span class="fa fa-paper-plane mr-1"></span> youremail@email.com</a>
+                            </p>
+                        </div>
+
+                        <div class="col-md-6 d-flex justify-content-md-end align-items-center">
+                            <div class="social-media mr-3">
+                                <p class="mb-0 d-flex">
+                                    <a href="#" class="d-flex align-items-center justify-content-center"><span
+                                            class="fa fa-facebook"></span></a>
+                                    <a href="#" class="d-flex align-items-center justify-content-center"><span
+                                            class="fa fa-twitter"></span></a>
+                                    <a href="#" class="d-flex align-items-center justify-content-center"><span
+                                            class="fa fa-instagram"></span></a>
+                                    <a href="#" class="d-flex align-items-center justify-content-center"><span
+                                            class="fa fa-dribbble"></span></a>
+                                </p>
+                            </div>
+
+                            {{-- 🌐 Language Switcher --}}
+                            <div class="language-switcher">
+                                @foreach (LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                                    <a class="ml-2"
+                                        href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
+                                        @if ($localeCode == 'en')
+                                            🇬🇧 EN
+                                        @elseif($localeCode == 'sv')
+                                            🇸🇪 SV
+                                        @else
+                                            {{ strtoupper($localeCode) }}
+                                        @endif
+                                    </a>
+                                @endforeach
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
-         </div>
-     </div>
- </nav>
- <!-- END nav -->
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+    <div class="container">
+        <a class="navbar-brand" href="{{ LaravelLocalization::localizeURL(route('home')) }}">Accounting</a>
+        <form action="#" class="searchform order-sm-start order-lg-last">
+            <div class="form-group d-flex">
+                <input type="text" class="form-control pl-3" placeholder="Search">
+                <button type="submit" class="form-control search"><span class="fa fa-search"></span></button>
+            </div>
+        </form>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
+            aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="fa fa-bars"></span> Menu
+        </button>
+
+        <div class="collapse navbar-collapse" id="ftco-nav">
+            <ul class="navbar-nav m-auto">
+
+                <li class="nav-item {{ Route::is('home') ? 'active' : '' }}">
+                    <a href="{{ route('home') }}" class="nav-link">Home</a>
+                </li>
+
+                <li class="nav-item {{ Route::is('about') ? 'active' : '' }}">
+                    <a href="{{ route('about') }}" class="nav-link">About</a>
+                </li>
+
+                <li class="nav-item {{ Route::is('services') ? 'active' : '' }}">
+                    <a href="{{ route('services') }}" class="nav-link">Services</a>
+                </li>
+
+                <li class="nav-item {{ Route::is('cases') ? 'active' : '' }}">
+                    <a href="{{ route('cases') }}" class="nav-link">Case Study</a>
+                </li>
+
+                <li class="nav-item {{ Route::is('blog') ? 'active' : '' }}">
+                    <a href="{{ route('blog') }}" class="nav-link">Blog</a>
+                </li>
+
+                <li class="nav-item {{ Route::is('contact') ? 'active' : '' }}">
+                    <a href="{{ route('contact') }}" class="nav-link">Contact</a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,26 @@
 <?php
 
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [
+            'localize',
+            'localizationRedirect',
+            'localeSessionRedirect',
+            'localeViewPath'
+        ]
+    ],
+    function () {
 
-Route::get('/contact', function () {
-    return view('sections.contact');
-})->name('contact');
-Route::get('/index', function () {
-    return view('sections.index');
-})->name('iinndex');
-
-Route::get('/blog', function () {
-    return view('sections.blog');
-})->name('blog');
-
-Route::get('/about', function () {
-    return view('sections.about');
-})->name('about');
-
-Route::get('/service', function () {
-    return view('sections.services');
-})->name('services');
-
-Route::get('/case-study', function () {
-    return view('sections.cases');
-})->name('cases');
+        Route::get('/', fn() => view('home'))->name('home');
+        Route::get('/contact', fn() => view('sections.contact'))->name('contact');
+        Route::get('/index', fn() => view('sections.index'))->name('iinndex');
+        Route::get('/blog', fn() => view('sections.blog'))->name('blog');
+        Route::get('/about', fn() => view('sections.about'))->name('about');
+        Route::get('/service', fn() => view('sections.services'))->name('services');
+        Route::get('/case-study', fn() => view('sections.cases'))->name('cases');
+    }
+);
