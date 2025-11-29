591bad889b529a67015aea3e5258466a8eb3521e
najjaf <najaf2961@gmail.com>
2025-11-29 21:28:04 +0500

    dff

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -19,13 +19,15 @@
 
     <link rel="stylesheet" href="{{ asset('assets/css/flaticon.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
 </head>
 
 <body>
 
     @include('layout.nav')
 
-@yield('content')
+    @yield('content')
 
     @include('layout.footer')
 
