3b78d192d1064d8602e58992b366a59ef8d0810e
najjaf <najaf2961@gmail.com>
2025-11-29 22:06:25 +0500

    sd

--- resources/lang/sv/app.php
+++ resources/lang/sv/app.php
@@ -1,15 +1,22 @@
 <?php
 
 return [
-    'phone' => '+00 1234 567', // Keep same format if number doesn’t change
-    'email' => 'youremail@email.com',
-    'search_placeholder' => 'Sök',
-    'menu' => 'Meny',
     'home' => 'Hem',
-    'about' => 'Om oss',
-    'services' => 'Tjänster',
-    'case_study' => 'Fallstudie',
-    'blog' => 'Blogg',
-    'contact' => 'Kontakt',
-    'brand' => 'Redovisning',
+    'contact_us' => 'Kontakta oss',
+    'get_in_touch' => 'Ta kontakt',
+    'message_sent' => 'Ditt meddelande har skickats, tack!',
+    'full_name' => 'Fullständigt namn',
+    'email_address' => 'E-postadress',
+    'subject' => 'Ämne',
+    'message' => 'Meddelande',
+    'send_message' => 'Skicka meddelande',
+    'lets_get_in_touch' => 'Låt oss ta kontakt',
+    'contact_intro' => 'Vi är öppna för förslag eller bara för att prata',
+    'address' => 'Adress:',
+    'phone' => 'Telefon:',
+    'email' => 'E-post:',
+    'website' => 'Webbplats',
+    'signup_consultation' => 'Anmäl dig för din kostnadsfria första bokföringskonsultation',
+    'enter_email' => 'Ange e-postadress',
+    'subscribe' => 'Prenumerera',
 ];
--- resources/views/sections/contact.blade.php
+++ resources/views/sections/contact.blade.php
@@ -8,11 +8,11 @@
             <div class="row no-gutters slider-text align-items-end">
                 <div class="col-md-9 ftco-animate pb-5">
                     <p class="breadcrumbs mb-2">
-                        <span class="mr-2"><a href="{{ url('/') }}">Home <i
+                        <span class="mr-2"><a href="{{ url('/') }}">{{ __('app.home') }} <i
                                     class="ion-ios-arrow-forward"></i></a></span>
-                        <span>Contact Us <i class="ion-ios-arrow-forward"></i></span>
+                        <span>{{ __('app.contact_us') }} <i class="ion-ios-arrow-forward"></i></span>
                     </p>
-                    <h1 class="mb-0 bread">Contact Us</h1>
+                    <h1 class="mb-0 bread">{{ __('app.contact_us') }}</h1>
                 </div>
             </div>
         </div>
@@ -26,44 +26,45 @@
                         <div class="row no-gutters">
                             <div class="col-lg-8 col-md-7 order-md-last d-flex align-items-stretch">
                                 <div class="contact-wrap w-100 p-md-5 p-4">
-                                    <h3 class="mb-4">Get in touch</h3>
+                                    <h3 class="mb-4">{{ __('app.get_in_touch') }}</h3>
                                     <div id="form-message-warning" class="mb-4"></div>
-                                    <div id="form-message-success" class="mb-4">
-                                        Your message was sent, thank you!
-                                    </div>
+                                    <div id="form-message-success" class="mb-4">{{ __('app.message_sent') }}</div>
                                     <form method="POST" id="contactForm" name="contactForm" class="contactForm">
                                         @csrf
                                         <div class="row">
                                             <div class="col-md-6">
                                                 <div class="form-group">
-                                                    <label class="label" for="name">Full Name</label>
+                                                    <label class="label" for="name">{{ __('app.full_name') }}</label>
                                                     <input type="text" class="form-control" name="name" id="name"
-                                                        placeholder="Name">
+                                                        placeholder="{{ __('app.full_name') }}">
                                                 </div>
                                             </div>
                                             <div class="col-md-6">
                                                 <div class="form-group">
-                                                    <label class="label" for="email">Email Address</label>
+                                                    <label class="label"
+                                                        for="email">{{ __('app.email_address') }}</label>
                                                     <input type="email" class="form-control" name="email" id="email"
-                                                        placeholder="Email">
+                                                        placeholder="{{ __('app.email_address') }}">
                                                 </div>
                                             </div>
                                             <div class="col-md-12">
                                                 <div class="form-group">
-                                                    <label class="label" for="subject">Subject</label>
+                                                    <label class="label" for="subject">{{ __('app.subject') }}</label>
                                                     <input type="text" class="form-control" name="subject" id="subject"
-                                                        placeholder="Subject">
+                                                        placeholder="{{ __('app.subject') }}">
                                                 </div>
                                             </div>
                                             <div class="col-md-12">
                                                 <div class="form-group">
-                                                    <label class="label" for="#">Message</label>
-                                                    <textarea name="message" class="form-control" id="message" cols="30" rows="4" placeholder="Message"></textarea>
+                                                    <label class="label" for="message">{{ __('app.message') }}</label>
+                                                    <textarea name="message" class="form-control" id="message" cols="30" rows="4"
+                                                        placeholder="{{ __('app.message') }}"></textarea>
                                                 </div>
                                             </div>
                                             <div class="col-md-12">
                                                 <div class="form-group">
-                                                    <input type="submit" value="Send Message" class="btn btn-primary">
+                                                    <input type="submit" value="{{ __('app.send_message') }}"
+                                                        class="btn btn-primary">
                                                     <div class="submitting"></div>
                                                 </div>
                                             </div>
@@ -71,16 +72,18 @@
                                     </form>
                                 </div>
                             </div>
+
                             <div class="col-lg-4 col-md-5 d-flex align-items-stretch">
                                 <div class="info-wrap bg-primary w-100 p-md-5 p-4">
-                                    <h3>Let's get in touch</h3>
-                                    <p class="mb-4">We're open for any suggestion or just to have a chat</p>
+                                    <h3>{{ __('app.lets_get_in_touch') }}</h3>
+                                    <p class="mb-4">{{ __('app.contact_intro') }}</p>
                                     <div class="dbox w-100 d-flex align-items-start">
                                         <div class="icon d-flex align-items-center justify-content-center">
                                             <span class="fa fa-map-marker"></span>
                                         </div>
                                         <div class="text pl-3">
-                                            <p><span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016</p>
+                                            <p><span>{{ __('app.address') }}</span> 198 West 21th Street, Suite 721 New
+                                                York NY 10016</p>
                                         </div>
                                     </div>
                                     <div class="dbox w-100 d-flex align-items-center">
@@ -88,7 +91,8 @@
                                             <span class="fa fa-phone"></span>
                                         </div>
                                         <div class="text pl-3">
-                                            <p><span>Phone:</span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
+                                            <p><span>{{ __('app.phone') }}</span> <a href="tel://1234567920">+ 1235 2355
+                                                    98</a></p>
                                         </div>
                                     </div>
                                     <div class="dbox w-100 d-flex align-items-center">
@@ -96,8 +100,8 @@
                                             <span class="fa fa-paper-plane"></span>
                                         </div>
                                         <div class="text pl-3">
-                                            <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@yoursite.com</a>
-                                            </p>
+                                            <p><span>{{ __('app.email') }}</span> <a
+                                                    href="mailto:info@yoursite.com">info@yoursite.com</a></p>
                                         </div>
                                     </div>
                                     <div class="dbox w-100 d-flex align-items-center">
@@ -105,11 +109,12 @@
                                             <span class="fa fa-globe"></span>
                                         </div>
                                         <div class="text pl-3">
-                                            <p><span>Website</span> <a href="#">yoursite.com</a></p>
+                                            <p><span>{{ __('app.website') }}</span> <a href="#">yoursite.com</a></p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+
                         </div>
                     </div>
                 </div>
@@ -123,14 +128,14 @@
         <div class="container py-5">
             <div class="row">
                 <div class="col-md-7 d-flex align-items-center">
-                    <h2 class="mb-3 mb-sm-0" style="color:black; font-size: 22px;">Sign Up for Your Free 1st Accounting
-                        Consultation</h2>
+                    <h2 class="mb-3 mb-sm-0" style="color:black; font-size: 22px;">{{ __('app.signup_consultation') }}
+                    </h2>
                 </div>
                 <div class="col-md-5 d-flex align-items-center">
                     <form action="#" class="subscribe-form">
                         <div class="form-group d-flex">
-                            <input type="text" class="form-control" placeholder="Enter email address">
-                            <input type="submit" value="Subscribe" class="submit px-3">
+                            <input type="text" class="form-control" placeholder="{{ __('app.enter_email') }}">
+                            <input type="submit" value="{{ __('app.subscribe') }}" class="submit px-3">
                         </div>
                     </form>
                 </div>
