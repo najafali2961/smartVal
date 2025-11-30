868b635bd3a8d22811bb3ad3fdd96f36bcc5cfab
najjaf <najaf2961@gmail.com>
2025-11-30 19:29:49 +0500

    dfg

---
+++ resources/views/sections/info.blade.php
@@ -0,0 +1,22 @@
+ <section class="ftco-section ftco-no-pt bg-light">
+     <div class="container">
+         <div class="row d-flex no-gutters">
+             <div class="col-md-6 d-flex">
+                 <div class="img img-video d-flex align-self-stretch align-items-center justify-content-center justify-content-md-center mb-4 mb-sm-0"
+                     style="background-image: url('{{ asset('assets/images/about.jpg') }}');">
+                 </div>
+             </div>
+             <div class="col-md-6 pl-md-5 py-md-5">
+                 <div class="heading-section pl-md-4 pt-md-5">
+                     <span class="subheading">Welcome to Accounting</span>
+                     <h2 class="mb-4">We Are the Best Accounting Agency</h2>
+                     <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                         there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the
+                         Semantics, a large language ocean.</p>
+                     <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.
+                         It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </section>
--- resources/views/sections/services.blade.php
+++ resources/views/sections/services.blade.php
@@ -19,62 +19,8 @@
         </div>
     </section>
 
-    <section class="ftco-section">
-        <div class="container">
-            <div class="row">
 
-                <div class="col-md-6 col-lg-3 d-flex services align-self-stretch px-4 ftco-animate">
-                    <div class="d-block">
-                        <div class="icon d-flex mr-2">
-                            <span class="flaticon-accounting-1"></span>
-                        </div>
-                        <div class="media-body">
-                            <h3 class="heading">{{ __('app.service_accounting_title') }}</h3>
-                            <p>{{ __('app.service_accounting_text') }}</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-6 col-lg-3 d-flex services align-self-stretch px-4 ftco-animate">
-                    <div class="d-block">
-                        <div class="icon d-flex mr-2">
-                            <span class="flaticon-tax"></span>
-                        </div>
-                        <div class="media-body">
-                            <h3 class="heading">{{ __('app.service_tax_title') }}</h3>
-                            <p>{{ __('app.service_tax_text') }}</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-6 col-lg-3 d-flex services align-self-stretch px-4 ftco-animate">
-                    <div class="d-block">
-                        <div class="icon d-flex mr-2">
-                            <span class="flaticon-loan"></span>
-                        </div>
-                        <div class="media-body">
-                            <h3 class="heading">{{ __('app.service_finance_title') }}</h3>
-                            <p>{{ __('app.service_finance_text') }}</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-6 col-lg-3 d-flex services align-self-stretch px-4 ftco-animate">
-                    <div class="d-block">
-                        <div class="icon d-flex mr-2">
-                            <span class="flaticon-budget"></span>
-                        </div>
-                        <div class="media-body">
-                            <h3 class="heading">{{ __('app.service_growth_title') }}</h3>
-                            <p>{{ __('app.service_growth_text') }}</p>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </section>
-
+    @include('sections.acounting')
     @include('sections.faq')
     @include('sections.pricing')
 @endsection
