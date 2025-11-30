9a8952dce128672397b806997ca5406b53bd0683
najjaf <najaf2961@gmail.com>
2025-11-30 18:32:26 +0500

    ds

--- public/assets/js/google-map.js
+++ public/assets/js/google-map.js
@@ -1,62 +1,21 @@
+// Wait for page to load
+document.addEventListener("DOMContentLoaded", function () {
+    // Center on Sweden (Stockholm area)
+    var map = L.map('map').setView([59.3293, 18.0686], 6); // 6 = shows whole Sweden nicely
 
-var google;
+    // Free OpenStreetMap tiles (no API key needed!)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
 
-function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-    // 39.399872
-    // -8.224454
+    // Add a nice marker in Stockholm
+    L.marker([59.3293, 18.0686])
+        .addTo(map)
+        .bindPopup("<b>We are in Sweden!</b><br>Stockholm")
+        .openPopup();
 
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 7,
-
-        // The latitude and longitude to center the map (always required)
-        center: myLatlng,
-
-        // How you would like to style the map.
-        scrollwheel: false,
-        styles: [
-            {
-                "featureType": "administrative.country",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "visibility": "simplified"
-                    },
-                    {
-                        "hue": "#ff0000"
-                    }
-                ]
-            }
-        ]
-    };
-
-
-
-    // Get the HTML DOM element that will contain your map
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
-
-    // Create the Google Map using out element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-
-    var addresses = ['New York'];
-
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address=' + addresses[x] + '&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
-            });
-
-        });
-    }
-
-}
-google.maps.event.addDomListener(window, 'load', init);
+    // Optional: Add more cities
+    L.marker([57.7089, 11.9746]).addTo(map).bindPopup("Gothenburg");
+    L.marker([55.6049, 13.0038]).addTo(map).bindPopup("Malmö");
+    L.marker([63.8258, 20.2630]).addTo(map).bindPopup("Umeå");
+});
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -20,7 +20,8 @@
     <link rel="stylesheet" href="{{ asset('assets/css/flaticon.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
 
 </head>
 
